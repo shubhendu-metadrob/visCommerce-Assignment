8d31e650136aafe7c37be48e34873bcd0451c8c9
shubhendu-metadrob <shubhendu@metadrob.com>
2024-11-25 15:56:37 +0530

    #build stash

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://viscommerce-chat.onrender.com/',
         secure: false,
       }
     }
